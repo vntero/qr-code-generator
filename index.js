b047560e8ed1b3e451d379cee9a9b9ed3ccb580b
vntero <shin.antero@gmail.com>
2023-12-30 17:30:33 +0100

    qr image is generated as expected

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-import inquirer from 'inquirer';
+import inquirer from 'inquirer'
+import qr from 'qr-image'
+import fs from 'fs'
 
 inquirer
   .prompt([
@@ -8,7 +10,9 @@ inquirer
     }
   ])
   .then((answers) => {
-    console.log(answers)
+    const url = answers.URL 
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream('qr_img.png'))
   })
   .catch((error) => {
     if (error.isTtyError) {
